78cdd15b0db9f561142c9421b92405e6cf62925f
shinyumin123 <shinyumin0507@gmail.com>
2024-04-11 20:09:22 +0900

    유민1차

---
+++ src/main/java/likelion/helloworld/controller/hello.java
@@ -0,0 +1,37 @@
+package likelion.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    @GetMapping("/hello-data")
+    public String helloData(Model model) {
+        model.addAttribute("nameKey", "유민~");
+        return "hello";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloPath(@PathVariable String name, Model model) {
+        model.addAttribute("nameKey", name);
+        return "hello";
+    }
+    @GetMapping("/hello-param")
+    public String helloParam(@RequestParam("name2") String name,
+                             @RequestParam("age")int age,
+                             @RequestParam("major") String major, Model model) {
+        model.addAttribute("nameKey", name);
+        model.addAttribute("ageKey", age);
+        model.addAttribute("majorKey", major);
+
+        return "hello";
+    }
+}
