ef2906f253115bc98db707ca1865999ac3ccf482
chtw2001 <jungtaekwon1019@gmail.com>
2024-04-12 23:12:15 +0900

    fix all

--- src/main/java/likelion/helloworld/controller/juhee.java
+++ src/main/java/likelion/helloworld/controller/juhee.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 // test
 @Controller
 public class juhee {
-
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
+
     @GetMapping("/hello-data")
     public String helloData(Model model) {
-        model.addAttribute("nameKey", "유민~");
+        model.addAttribute("nameKey", "노주희");
         return "hello";
     }
 
@@ -23,19 +23,32 @@ public class juhee {
     public String helloPath(@PathVariable String name, Model model) {
         model.addAttribute("nameKey", name);
         return "hello";
-
     }
-    @GetMapping("/yumin-param")
-    public String yuminParam(@RequestParam("name") String name,
-                             @RequestParam("age") int age,
-                             @RequestParam("MBTI") String mbti,
-                             @RequestParam("hobby") String game,
-                             @RequestParam("major") String major, Model model) {
+
+    @GetMapping("/hello-param")
+    public String helloParam(@RequestParam("name2") String name,
+                             @RequestParam("age2") int age,
+                             @RequestParam("major") String major,
+                             Model model) {
         model.addAttribute("nameKey", name);
         model.addAttribute("ageKey", age);
-        model.addAttribute("MBTIKey", mbti);
-        model.addAttribute("hobbyKey", game);
         model.addAttribute("majorKey", major);
-        return "yumin";
+        return "hello";
     }
+
+    @GetMapping("/hello-juhee")
+    public String helloJuhee(@RequestParam("name") String name,
+                             @RequestParam("age") int age,
+                             @RequestParam("major") String major,
+                             @RequestParam("MBTI") String mbti,
+                             @RequestParam("hobby") String hobby,
+                             Model model) {
+        model.addAttribute("nameKey", name);
+        model.addAttribute("ageKey", age);
+        model.addAttribute("majorKey", major);
+        model.addAttribute("MBTIKey", mbti);
+        model.addAttribute("hobbyKey", hobby);
+        return "juhee";
+    }
+
 }
---
+++ src/main/java/likelion/helloworld/controller/yumin.java
@@ -0,0 +1,38 @@
+package likelion.helloworld.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class yumin {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    @GetMapping("/hello-data")
+    public String helloData(Model model) {
+        model.addAttribute("nameKey", "유민~");
+        return "hello";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloPath(@PathVariable String name, Model model) {
+        model.addAttribute("nameKey", name);
+        return "hello";
+
+    }
+    @GetMapping("/yumin-param")
+    public String yuminParam(@RequestParam("name") String name,
+                             @RequestParam("age") int age,
+                             @RequestParam("MBTI") String mbti,
+                             @RequestParam("hobby") String game,
+                             @RequestParam("major") String major, Model model) {
+        model.addAttribute("nameKey", name);
+        model.addAttribute("ageKey", age);
+        model.addAttribute("MBTIKey", mbti);
+        model.addAttribute("hobbyKey", game);
+        model.addAttribute("majorKey", major);
+        return "yumin";
+    }
+}
